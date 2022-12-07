269fc7af7ff6fc060bde292f60804f83ee3d1247
AnimewkaSes <brol0526@gmail.com>
2022-12-08 02:29:31 +0600

    Задача 25

---
+++ EX025/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Определение степение числа");
+        Console.Write("Введите число: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите степень: ");
+        var B = Convert.ToInt32(Console.ReadLine());
+        int result = 1;
+     for (int i = 1; i <= B; i++)
+     {
+       result*=A;
+     }
+      Console.WriteLine("Число {0}^{1} = {2}", A, B, result);
+      Console.ReadKey();
+}
+}
