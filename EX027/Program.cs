98257d5226190d6259cf09f5003d4a2ae1385950
AnimewkaSes <brol0526@gmail.com>
2022-12-10 00:52:21 +0600

    Задача 27

---
+++ EX027/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
+        Console.Write("Введите длину числа: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        double[] number = new double[n];
+        Console.WriteLine("Введите число");
+        for (int i = 0; i < number.Length; i++)
+        {
+             number[i] =  Convert.ToDouble(Console.ReadLine());
+        }
+        Console.Write("Введенное число: ");
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.Write("{0} ",number[i]);
+        }
+        double result= 0;
+        foreach (double item in number){
+            result +=item; 
+        }
+        Console.Write("\nCумма элементов числа: {0}",result);
+    }
+}
