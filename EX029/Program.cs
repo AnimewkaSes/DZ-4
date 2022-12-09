b27bf6b6011aa0f890eb13b0d4dc4283ff52770a
AnimewkaSes <brol0526@gmail.com>
2022-12-10 01:27:06 +0600

    Задача 29

---
+++ EX029/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите количество генерируемых чисел: ");
+       int n = Convert.ToInt32(Console.ReadLine());
+       int [] arr = new int [n];
+       Random rnd = new Random();
+       Console.Write("Сгенерируемый массив: ");
+       for (int i = 0; i < arr.Length; i++)
+       {
+            arr[i]=rnd.Next(-20,20);
+       }
+
+       for (int i = 0; i < arr.Length; i++)
+       {
+        Console.Write("{0}, ",arr[i]);
+       }
+
+        Array.Sort(arr);
+        Console.Write("\nОтсортированный по модулю массив: ");
+        foreach (int a in arr)
+        {
+            Console.Write("{0}, ",a);
+        }
+    }
+}
